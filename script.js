4962968879085bcfbc06e14f539f0d0d0339031d
HutaoMain <alimohamedalcantara@gmail.com>
2023-09-16 22:32:50 +0800

    don

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Your JavaScript code here
-
   const serviceTypes = [
     "Tarpaulin",
     "Neon LED Lights",
@@ -29,20 +27,34 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function updateServiceType() {
     const serviceTypeSelect = document.getElementById("serviceType");
-    const tarpaulinDescription = document.getElementById(
-      "tarpaulinDescription"
-    );
-    const laminateDescription = document.getElementById("laminateDescription");
+
+    const serviceSections = {
+      Tarpaulin: "tarpaulinSection",
+      "Neon LED Lights": "neonLedLightsSection",
+      Panaflex: "panaflexSection",
+      Laser: "laserSection",
+      Acrylic: "acrylicSection",
+      Vinyl: "vinylSection",
+      Risograph: "risoGraphSection",
+      "Sintra Board": "sintraBoardSection",
+      Sticker: "stickerSection",
+      "T-shirt": "tshirtSection",
+      Laminate: "LaminateSection",
+      Lanyard: "lanyardSection",
+    };
 
     const selectedServiceType = serviceTypeSelect.value;
 
-    tarpaulinDescription.style.display = "none";
-    laminateDescription.style.display = "none";
+    for (const sectionId of Object.values(serviceSections)) {
+      const section = document.getElementById(sectionId);
+      if (section) {
+        section.style.display = "none";
+      }
+    }
 
-    if (selectedServiceType === "Tarpaulin") {
-      tarpaulinDescription.style.display = "flex";
-    } else if (selectedServiceType === "Laminate") {
-      laminateDescription.style.display = "flex";
+    const selectedSectionId = serviceSections[selectedServiceType];
+    if (selectedSectionId) {
+      document.getElementById(selectedSectionId).style.display = "block";
     }
   }
 
@@ -53,4 +65,184 @@ document.addEventListener("DOMContentLoaded", function () {
   populateServiceTypeSelect();
 
   updateServiceType();
+});
+
+// tarpaulin
+document.addEventListener("DOMContentLoaded", function () {
+  function loadTarpaulinSection() {
+    fetch("./sections/tarpaulin.html")
+      .then((response) => response.text())
+      .then((html) => {
+        document.getElementById("tarpaulinSection").innerHTML = html;
+      })
+      .catch((error) => {
+        console.error("Error loading tarpaulin section:", error);
+      });
+  }
+  loadTarpaulinSection();
+});
+
+// neon led lights
+document.addEventListener("DOMContentLoaded", function () {
+  function loadNeonLightsSection() {
+    fetch("./sections/neonLedLights.html")
+      .then((response) => response.text())
+      .then((html) => {
+        document.getElementById("neonLedLightsSection").innerHTML = html;
+      })
+      .catch((error) => {
+        console.error("Error loading neonLedLightsSection section:", error);
+      });
+  }
+  loadNeonLightsSection();
+});
+
+// panaflex
+document.addEventListener("DOMContentLoaded", function () {
+  function loadPanaflexSection() {
+    fetch("./sections/panaflex.html")
+      .then((response) => response.text())
+      .then((html) => {
+        document.getElementById("panaflexSection").innerHTML = html;
+      })
+      .catch((error) => {
+        console.error("Error loading panaflexSection section:", error);
+      });
+  }
+  loadPanaflexSection();
+});
+
+// laser
+document.addEventListener("DOMContentLoaded", function () {
+  function loadLaserSection() {
+    fetch("./sections/laser.html")
+      .then((response) => response.text())
+      .then((html) => {
+        document.getElementById("laserSection").innerHTML = html;
+      })
+      .catch((error) => {
+        console.error("Error loading laserSection section:", error);
+      });
+  }
+  loadLaserSection();
+});
+
+// acrylic
+document.addEventListener("DOMContentLoaded", function () {
+  function loadAcrylicSection() {
+    fetch("./sections/acrylic.html")
+      .then((response) => response.text())
+      .then((html) => {
+        document.getElementById("acrylicSection").innerHTML = html;
+      })
+      .catch((error) => {
+        console.error("Error loading acrylicSection section:", error);
+      });
+  }
+  loadAcrylicSection();
+});
+
+// vinyl
+document.addEventListener("DOMContentLoaded", function () {
+  function loadVinylSection() {
+    fetch("./sections/vinyl.html")
+      .then((response) => response.text())
+      .then((html) => {
+        document.getElementById("vinylSection").innerHTML = html;
+      })
+      .catch((error) => {
+        console.error("Error loading vinylSection section:", error);
+      });
+  }
+  loadVinylSection();
+});
+
+// risograph
+document.addEventListener("DOMContentLoaded", function () {
+  function loadRisoGraphSection() {
+    fetch("./sections/risograph.html")
+      .then((response) => response.text())
+      .then((html) => {
+        document.getElementById("risoGraphSection").innerHTML = html;
+      })
+      .catch((error) => {
+        console.error("Error loading risoGraphSection section:", error);
+      });
+  }
+  loadRisoGraphSection();
+});
+
+// sintraboard
+document.addEventListener("DOMContentLoaded", function () {
+  function loadSintraBoardSection() {
+    fetch("./sections/sintraBoard.html")
+      .then((response) => response.text())
+      .then((html) => {
+        document.getElementById("sintraBoardSection").innerHTML = html;
+      })
+      .catch((error) => {
+        console.error("Error loading sintraBoardSection section:", error);
+      });
+  }
+  loadSintraBoardSection();
+});
+
+// sticker
+document.addEventListener("DOMContentLoaded", function () {
+  function loadStickerSection() {
+    fetch("./sections/sticker.html")
+      .then((response) => response.text())
+      .then((html) => {
+        document.getElementById("stickerSection").innerHTML = html;
+      })
+      .catch((error) => {
+        console.error("Error loading stickerSection section:", error);
+      });
+  }
+  loadStickerSection();
+});
+
+// tshirt
+document.addEventListener("DOMContentLoaded", function () {
+  function loadTshirtSection() {
+    fetch("./sections/tshirt.html")
+      .then((response) => response.text())
+      .then((html) => {
+        document.getElementById("tshirtSection").innerHTML = html;
+      })
+      .catch((error) => {
+        console.error("Error loading tshirtSection section:", error);
+      });
+  }
+  loadTshirtSection();
+});
+
+// laminate
+document.addEventListener("DOMContentLoaded", function () {
+  function loadLaminateSection() {
+    fetch("./sections/laminate.html")
+      .then((response) => response.text())
+      .then((html) => {
+        document.getElementById("LaminateSection").innerHTML = html;
+      })
+      .catch((error) => {
+        console.error("Error loading LaminateSection section:", error);
+      });
+  }
+  loadLaminateSection();
+});
+
+// laminate
+document.addEventListener("DOMContentLoaded", function () {
+  function loadLanyardSection() {
+    fetch("./sections/lanyard.html")
+      .then((response) => response.text())
+      .then((html) => {
+        document.getElementById("lanyardSection").innerHTML = html;
+      })
+      .catch((error) => {
+        console.error("Error loading lanyardSection section:", error);
+      });
+  }
+  loadLanyardSection();
 });
